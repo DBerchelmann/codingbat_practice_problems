a03dc4b0c79ab4edffd650c3ee363447fdef1b0a
David Berchelmann <david.a.berchelmann@gmail.com>
2020-12-19 15:50:55 -0600

    add new python string file, update warmup

---
+++ string-1-exercises.py
@@ -0,0 +1,20 @@
+# Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+
+#hello_name('Bob') → 'Hello Bob!'
+#hello_name('Alice') → 'Hello Alice!'
+#hello_name('X') → 'Hello X!'
+
+def hello_name(name):
+  return 'Hello ' + name + '!'
+
+
+# Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+
+#make_abba('Hi', 'Bye') → 'HiByeByeHi'
+#make_abba('Yo', 'Alice') → 'YoAliceAliceYo'
+#make_abba('What', 'Up') → 'WhatUpUpWhat'
+
+def make_abba(a, b):
+  return a + b + b + a
--- warmup_python.py
+++ warmup_python.py
@@ -14,3 +14,30 @@ def parrot_trouble(talking, hour):
   else:
     return False
 
+ 
+# Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+
+
+# makes10(9, 10) → True
+# makes10(9, 9) → False
+# makes10(1, 9) → True
+
+def makes10(a, b):
+  return (a == 10 or b == 10 or a+b == 10)
+ 
+
+
+# Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
+
+
+# not_string('candy') → 'not candy'
+# Anot_string('x') → 'not x'
+# not_string('not bad') → 'not bad'
+
+def not_string(str):
+    if len(str) >= 3 and str[:3] == "not":
+        return str
+    return "not " + str
+    
+  # str[:3] goes from the start of the string up to but not
+  # including index 3
