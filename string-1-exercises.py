190d47604486fb007f837006e6356a544f5f3556
David Berchelmann <david.a.berchelmann@gmail.com>
2020-12-19 17:37:13 -0600

    added more work

--- string-1-exercises.py
+++ string-1-exercises.py
@@ -28,3 +28,16 @@ def make_abba(a, b):
 
 def make_tags(tag, word):
   return '<' + tag + '>' + word + '</' + tag + '>'
+
+
+
+# Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>".
+
+
+# make_out_word('<<>>', 'Yay') → '<<Yay>>'
+# make_out_word('<<>>', 'WooHoo') → '<<WooHoo>>'
+# make_out_word('[[]]', 'word') → '[[word]]'
+
+def make_out_word(out, word):
+  return out[:2] + word + out [2:]
+
