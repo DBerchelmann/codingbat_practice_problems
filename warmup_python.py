ca2d588bfaa33f7c4def4bf9d5636607f1a5f2f7
David Berchelmann <david.a.berchelmann@gmail.com>
2020-12-19 23:21:47 -0600

    updated SQL exercises

--- warmup_python.py
+++ warmup_python.py
@@ -38,6 +38,36 @@ def not_string(str):
     if len(str) >= 3 and str[:3] == "not":
         return str
     return "not " + str
-    
+
   # str[:3] goes from the start of the string up to but not
   # including index 3
+
+
+# Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+
+# diff21(19) → 2
+# diff21(10) → 11
+# diff21(21) → 0
+
+def diff21(n):
+  if n < 21:
+    return 21 - n
+  else:
+    return (n - 21) * 2
+
+
+# Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the absolute value of a number.
+
+
+# near_hundred(93) → True
+# near_hundred(90) → True
+# near_hundred(89) → False
+
+def near_hundred(n):
+  if abs(100 - n) <= 10:
+    return True
+  elif  abs(200 - n) <= 10:
+    return True
+  else:
+    return False
